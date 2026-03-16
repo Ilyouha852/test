d79a56c20294d138d24ec144916f01c7a340a1c7
Ilyousha <ilaivanov2899@gmail.com>
2026-03-16 14:28:11 +0800

    Добавлен health-check

--- src/routes.ts
+++ src/routes.ts
@@ -2,4 +2,8 @@ import { Router } from 'express';
 
 export const apiV1Router = Router();
 
+apiV1Router.get('/health-check', (_req, res) => {
+    res.status(200).json({ status: 'OK' });
+});
+
 // apiV1Router.use('/health-check', healthCheckRoutes);
