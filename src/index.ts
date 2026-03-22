4d226cda68652f2f3755becc9c192cd982aba5e7
mrquw <mrquwerty@mail.ru>
2026-03-22 23:22:51 +0800

    Переписан index.ts, удален app.ts. Отключен MessengerAggregator

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import 'dotenv/config';
 import express from 'express';
 import morgan from 'morgan';
 
-import { MockConnector } from './modules/messenger-aggregator/connectors/mock-connector.js';
-import { messengerAggregator } from './modules/messenger-aggregator/messenger-aggregator.js';
 import { apiV1Router } from './routes.js';
 
 const serverPort: number = Number(process.env.PORT) || 3007;
@@ -12,8 +10,8 @@ const serverUrl: string = process.env.BOT_BASE_URL || 'localhost';
 
 const app = express();
 
+app.use(apiV1Router);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 const environment = process.env.NODE_ENV || 'development';
 
@@ -21,16 +19,10 @@ if (environment == 'development') {
     app.use(morgan('dev'));
 }
 
-messengerAggregator.registerConnector(new MockConnector());
-
-app.use(apiV1Router);
-
-app.use('*', (_req, res) => {
-    res.status(404).json({ error: 'Маршрут не найден' });
-});
-
 app.listen(serverPort, serverUrl, () => {
     console.log(`🚀 API Сервер запущен по адресу ${serverUrl}:${serverPort}`);
-    console.log(`   GET  /health-check`);
-    console.log(`   POST /user_message`);
+});
+
+app.use('*', (req, res) => {
+    res.status(404).json({ error: 'Маршрут не найден' });
 });
